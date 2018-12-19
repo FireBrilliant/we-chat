f9d3fa209932beba4067b983d29715d33d44cbab
zhangzhaohuang <firebrilliant@dingtalk.com>
2018-12-19 19:06:34 +0800

    project init

---
+++ src/pages/me/Me.vue
@@ -0,0 +1,18 @@
+<template>
+    <div class="me-page">
+      个人中心页面
+    </div>
+</template>
+
+<script>
+  export default {
+    name: 'Me',
+    created () {
+      console.log('jkk')
+    }
+  }
+</script>
+
+<style scoped>
+
+</style>
